b6942258e5afbec9003f5e790670b30fc5dbd58b
LePet1tPrince <95003031+LePet1tPrince@users.noreply.github.com>
2025-04-11 14:10:15 -0400

    Create manipulate_data.py
    created a stored procedure that we can access from the repo

---
+++ procedure/manipulate_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import snowflake.snowpark as snowpark
+
+def double_the_table(session: snowpark.Session):
+  df = session.sql("SELECT * FROM MY_GIT_TABLE").to_pandas()
+  df['NEW_COLUMN'] = 'HI I"M A Anew column'
+
+  df.write_pandas(df = df, schema="PUBLIC", table_name="NEW_GIT_TABLE", overwrite=True, auto_create_table=True)
+  return "SUCCESS"
