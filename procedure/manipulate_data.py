48b15673f20a5b061a06a2acccc0fb6f797162c2
LePet1tPrince <95003031+LePet1tPrince@users.noreply.github.com>
2025-04-11 15:13:09 -0400

    Update manipulate_data.py
    fixing pd to session.write_pandas

--- procedure/manipulate_data.py
+++ procedure/manipulate_data.py
@@ -6,5 +6,5 @@ def double_the_table(session: snowpark.Session):
   df = session.table("MY_GIT_TABLE").to_pandas()
   df['NEW_COLUMN'] = 'HI I"M A Anew column'
 
-  pd.write_pandas(df = df, schema="PUBLIC", table_name="NEW_GIT_TABLE", overwrite=True, auto_create_table=True)
+  session.write_pandas(df = df, schema="PUBLIC", table_name="NEW_GIT_TABLE", overwrite=True, auto_create_table=True)
   return "SUCCESS"
