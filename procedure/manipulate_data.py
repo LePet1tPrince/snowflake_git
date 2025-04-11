2d75fdfb7a44c11efdbb40a2ca20c37da1a16c90
LePet1tPrince <95003031+LePet1tPrince@users.noreply.github.com>
2025-04-11 15:14:54 -0400

    Update manipulate_data.py
    making a change to see if it automatically comes into the procedure

--- procedure/manipulate_data.py
+++ procedure/manipulate_data.py
@@ -4,7 +4,7 @@ import snowflake.snowpark as snowpark
 
 def double_the_table(session: snowpark.Session):
   df = session.table("MY_GIT_TABLE").to_pandas()
-  df['NEW_COLUMN'] = 'HI I"M A Anew column'
+  df['NEW_COLUMN'] = 'Making a change!!'
 
   session.write_pandas(df = df, schema="PUBLIC", table_name="NEW_GIT_TABLE", overwrite=True, auto_create_table=True)
   return "SUCCESS"
